c1a048d31a9eeb2c5fb19862799798604c7038c1
Felipe-Gs <felipe123321rei@gmail.com>
2024-03-19 12:13:32 -0300

    feat: "create project + native wind"

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@/styles/global.css";
+
+import { StatusBar } from "expo-status-bar";
+
+import { Profile } from "./src/app/Profile";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <>
+         <Profile />
+         <StatusBar style="auto" />
+      </>
+   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/app/Profile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export function Profile() {
+   return (
+      <View className="flex-1 justify-center items-center">
+         <Text>Profile</Text>
+         <Text style={{ flex: 1, justifyContent: "center" }}></Text>
+      </View>
+   );
+}
